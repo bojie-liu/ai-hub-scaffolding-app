7155a177b9c15f31656fffbfccccfcdd2820a562
joshliu <bojieliu711@gmail.com>
2026-04-14 16:54:00 +0800

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import { AppProvider } from "@/contexts/AppProvider";
 import { TokenInitializer } from "@/components/TokenInitializer";
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AppProvider>
-          <TokenInitializer />
+          <Suspense>
+            <TokenInitializer />
+          </Suspense>
           {children}
         </AppProvider>
       </body>
