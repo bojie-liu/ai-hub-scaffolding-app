77e8db68ebd81d53d54791c9fa5d98b38d4c9559
bjliu <bjliu@eduhk.hk>
2026-05-14 17:21:35 +0200

    Deploy project v0.0.1-test at 2026-05-14T15:21:35.380Z

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: '/config/.local/share/code-server/projects/14e98853-6387-4e42-a643-a6d97afc038d',
-  },
 };
 
 export default nextConfig;
